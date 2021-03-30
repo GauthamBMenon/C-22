75a29e94fe823b968b5c036b37b28482a44774fb
GauthamBMenon <79324557+GauthamBMenon@users.noreply.github.com>
2021-03-30 20:07:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies = Matter.Bodies
+
+
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() {
+  background("black");  
+  fill("white")
+  rectMode(CENTER)
+rect(200,200,50,50)
+}
